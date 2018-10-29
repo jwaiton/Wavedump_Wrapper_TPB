282d58bbfbcb99c13bf9aecc577b644554697594
Watchman-PMT <gary.smith@ed.ac.uk>
2018-10-29 11:16:02 +0000

    First working version of RunDaq.sh: automated SPE datra taking

--- Wrapper/SPE_Analysis/SPE_Gen.d
+++ Wrapper/SPE_Analysis/SPE_Gen.d
@@ -1,109 +1,73 @@
-SPE_Gen.o SPE_Gen.d: SPE_Gen.cpp /home/user1/Wave//src/ns.h \
- /home/user1/Wave//src/command_parse.h /home/user1/Wave//src/dist.h \
- /home/user1/Wave//src/file.h /home/user1/Wave//src/stats.h \
- /home/user1/Wave//src/utility.h \
- /home/user1/Software/build/include/TFile.h \
- /home/user1/Software/build/include/TDirectoryFile.h \
- /home/user1/Software/build/include/TDirectory.h \
- /home/user1/Software/build/include/TNamed.h \
- /home/user1/Software/build/include/TObject.h \
- /home/user1/Software/build/include/Rtypes.h \
- /home/user1/Software/build/include/RtypesCore.h \
- /home/user1/Software/build/include/RConfig.h \
- /home/user1/Software/build/include/RVersion.h \
- /home/user1/Software/build/include/DllImport.h \
- /home/user1/Software/build/include/Rtypeinfo.h \
- /home/user1/Software/build/include/snprintf.h \
- /home/user1/Software/build/include/strlcpy.h \
- /home/user1/Software/build/include/TGenericClassInfo.h \
- /home/user1/Software/build/include/TSchemaHelper.h \
- /home/user1/Software/build/include/TStorage.h \
- /home/user1/Software/build/include/TVersionCheck.h \
- /home/user1/Software/build/include/Riosfwd.h \
- /home/user1/Software/build/include/TBuffer.h \
- /home/user1/Software/build/include/TString.h \
- /home/user1/Software/build/include/TMathBase.h \
- /home/user1/Software/build/include/RStringView.h \
- /home/user1/Software/build/include/RConfigure.h \
- /home/user1/Software/build/include/RWrap_libcpp_string_view.h \
- /home/user1/Software/build/include/libcpp_string_view.h \
- /home/user1/Software/build/include/TList.h \
- /home/user1/Software/build/include/TSeqCollection.h \
- /home/user1/Software/build/include/TCollection.h \
- /home/user1/Software/build/include/TIterator.h \
- /home/user1/Software/build/include/TDatime.h \
- /home/user1/Software/build/include/TUUID.h \
- /home/user1/Software/build/include/TMap.h \
- /home/user1/Software/build/include/THashTable.h \
- /home/user1/Software/build/include/TUrl.h \
- /home/user1/Software/build/include/TH1D.h \
- /home/user1/Software/build/include/TH1.h \
- /home/user1/Software/build/include/TAxis.h \
- /home/user1/Software/build/include/TAttAxis.h \
- /home/user1/Software/build/include/TArrayD.h \
- /home/user1/Software/build/include/TArray.h \
- /home/user1/Software/build/include/TAttLine.h \
- /home/user1/Software/build/include/TAttFill.h \
- /home/user1/Software/build/include/TAttMarker.h \
- /home/user1/Software/build/include/TArrayC.h \
- /home/user1/Software/build/include/TArrayS.h \
- /home/user1/Software/build/include/TArrayI.h \
- /home/user1/Software/build/include/TArrayF.h \
- /home/user1/Software/build/include/Foption.h \
- /home/user1/Software/build/include/TVectorFfwd.h \
- /home/user1/Software/build/include/TVectorDfwd.h \
- /home/user1/Software/build/include/TFitResultPtr.h \
- /home/user1/Software/build/include/TH2D.h \
- /home/user1/Software/build/include/TH2.h \
- /home/user1/Software/build/include/TMatrixFBasefwd.h \
- /home/user1/Software/build/include/TMatrixDBasefwd.h \
- /home/user1/Software/build/include/TGraph.h \
- /home/user1/Software/build/include/TROOT.h /home/user1/Wave//src/atmap.h \
- /home/user1/Wave//src/ns.h \
- /home/user1/Software/build/include/TApplication.h \
- /home/user1/Software/build/include/TQObject.h \
- /home/user1/Software/build/include/TQObjectEmitVA.h \
- /home/user1/Software/build/include/TQConnection.h \
- /home/user1/Software/build/include/Varargs.h \
- /home/user1/Software/build/include/TInterpreter.h \
- /home/user1/Software/build/include/TDictionary.h \
- /home/user1/Software/build/include/ESTLType.h \
- /home/user1/Software/build/include/TVirtualMutex.h \
- /home/user1/Software/build/include/TApplicationImp.h \
- /home/user1/Software/build/include/TCanvas.h \
- /home/user1/Software/build/include/TPad.h \
- /home/user1/Software/build/include/TVirtualPad.h \
- /home/user1/Software/build/include/TAttPad.h \
- /home/user1/Software/build/include/TVirtualX.h \
- /home/user1/Software/build/include/TAttText.h \
- /home/user1/Software/build/include/GuiTypes.h \
- /home/user1/Software/build/include/Buttons.h \
- /home/user1/Software/build/include/TAttBBox2D.h \
- /home/user1/Software/build/include/TPoint.h \
- /home/user1/Software/build/include/TAttCanvas.h \
- /home/user1/Software/build/include/TCanvasImp.h \
- /home/user1/Software/build/include/TF1.h \
- /home/user1/Software/build/include/TFormula.h \
- /home/user1/Software/build/include/TBits.h \
- /home/user1/Software/build/include/TObjArray.h \
- /home/user1/Software/build/include/TMethodCall.h \
- /home/user1/Software/build/include/Math/ParamFunctor.h \
- /home/user1/Software/build/include/TFitResult.h \
- /home/user1/Software/build/include/Fit/FitResult.h \
- /home/user1/Software/build/include/Math/IFunctionfwd.h \
- /home/user1/Software/build/include/Math/IParamFunctionfwd.h \
- /home/user1/Software/build/include/TMatrixDSym.h \
- /home/user1/Software/build/include/TMatrixTSym.h \
- /home/user1/Software/build/include/TMatrixTBase.h \
- /home/user1/Software/build/include/TError.h \
- /home/user1/Software/build/include/TMatrixTUtils.h \
- /home/user1/Software/build/include/TMatrixDSymfwd.h \
- /home/user1/Software/build/include/TMatrixDUtils.h \
- /home/user1/Software/build/include/TMatrixDUtilsfwd.h \
- /home/user1/Software/build/include/TObject.h \
- /home/user1/Software/build/include/TGraphErrors.h \
- /home/user1/Software/build/include/TMultiGraph.h \
- /home/user1/Software/build/include/TF1.h \
- /home/user1/Software/build/include/TLegend.h \
- /home/user1/Software/build/include/TPave.h \
- /home/user1/Software/build/include/TBox.h
+SPE_Gen.o SPE_Gen.d: SPE_Gen.cpp src/atmap.h src/ns.h src/command_parse.h \
+ src/dist.h src/file.h src/stats.h src/utility.h \
+ /usr/local/include/root/TFile.h /usr/local/include/root/TDirectoryFile.h \
+ /usr/local/include/root/TDirectory.h /usr/local/include/root/TBuffer.h \
+ /usr/local/include/root/TObject.h /usr/local/include/root/RConfigure.h \
+ /usr/local/include/root/Rtypes.h /usr/local/include/root/RtypesCore.h \
+ /usr/local/include/root/RConfig.h /usr/local/include/root/RVersion.h \
+ /usr/local/include/root/DllImport.h /usr/local/include/root/snprintf.h \
+ /usr/local/include/root/strlcpy.h \
+ /usr/local/include/root/TGenericClassInfo.h \
+ /usr/local/include/root/TSchemaHelper.h \
+ /usr/local/include/root/TIsAProxy.h \
+ /usr/local/include/root/TVirtualIsAProxy.h \
+ /usr/local/include/root/TStorage.h \
+ /usr/local/include/root/TVersionCheck.h /usr/local/include/root/TNamed.h \
+ /usr/local/include/root/TString.h /usr/local/include/root/TMathBase.h \
+ /usr/local/include/root/RStringView.h \
+ /usr/local/include/root/RWrap_libcpp_string_view.h \
+ /usr/local/include/root/libcpp_string_view.h \
+ /usr/local/include/root/TList.h /usr/local/include/root/TSeqCollection.h \
+ /usr/local/include/root/TCollection.h \
+ /usr/local/include/root/TIterator.h /usr/local/include/root/TDatime.h \
+ /usr/local/include/root/TUUID.h /usr/local/include/root/TMap.h \
+ /usr/local/include/root/THashTable.h /usr/local/include/root/TUrl.h \
+ /usr/local/include/root/ROOT/TRWSpinLock.hxx \
+ /usr/local/include/root/ROOT/TSpinMutex.hxx \
+ /usr/local/include/root/TH1D.h /usr/local/include/root/TH1.h \
+ /usr/local/include/root/TAxis.h /usr/local/include/root/TAttAxis.h \
+ /usr/local/include/root/TArrayD.h /usr/local/include/root/TArray.h \
+ /usr/local/include/root/TAttLine.h /usr/local/include/root/TAttFill.h \
+ /usr/local/include/root/TAttMarker.h /usr/local/include/root/TArrayC.h \
+ /usr/local/include/root/TArrayS.h /usr/local/include/root/TArrayI.h \
+ /usr/local/include/root/TArrayF.h /usr/local/include/root/Foption.h \
+ /usr/local/include/root/TVectorFfwd.h \
+ /usr/local/include/root/TVectorDfwd.h \
+ /usr/local/include/root/TFitResultPtr.h /usr/local/include/root/TH2D.h \
+ /usr/local/include/root/TH2.h /usr/local/include/root/TMatrixFBasefwd.h \
+ /usr/local/include/root/TMatrixDBasefwd.h \
+ /usr/local/include/root/TGraph.h /usr/local/include/root/TROOT.h \
+ src/atmap.h src/command_parse.h src/dist.h src/file.h src/ns.h \
+ src/stats.h src/utility.h /usr/local/include/root/TApplication.h \
+ /usr/local/include/root/TQObject.h /usr/local/include/root/TError.h \
+ /usr/local/include/root/TVirtualQConnection.h \
+ /usr/local/include/root/TInterpreter.h \
+ /usr/local/include/root/TDictionary.h /usr/local/include/root/ESTLType.h \
+ /usr/local/include/root/TVirtualMutex.h \
+ /usr/local/include/root/TApplicationImp.h \
+ /usr/local/include/root/TCanvas.h /usr/local/include/root/TPad.h \
+ /usr/local/include/root/TVirtualPad.h /usr/local/include/root/TAttPad.h \
+ /usr/local/include/root/TVirtualX.h /usr/local/include/root/TAttText.h \
+ /usr/local/include/root/GuiTypes.h /usr/local/include/root/Buttons.h \
+ /usr/local/include/root/TAttBBox2D.h /usr/local/include/root/TPoint.h \
+ /usr/local/include/root/TAttCanvas.h \
+ /usr/local/include/root/TCanvasImp.h /usr/local/include/root/TF1.h \
+ /usr/local/include/root/TFormula.h /usr/local/include/root/TBits.h \
+ /usr/local/include/root/TObjArray.h \
+ /usr/local/include/root/TMethodCall.h \
+ /usr/local/include/root/Math/ParamFunctor.h \
+ /usr/local/include/root/TFitResult.h \
+ /usr/local/include/root/Fit/FitResult.h \
+ /usr/local/include/root/Math/IFunctionfwd.h \
+ /usr/local/include/root/Math/IParamFunctionfwd.h \
+ /usr/local/include/root/TMatrixDSym.h \
+ /usr/local/include/root/TMatrixTSym.h \
+ /usr/local/include/root/TMatrixTBase.h \
+ /usr/local/include/root/TMatrixTUtils.h \
+ /usr/local/include/root/TMatrixDSymfwd.h \
+ /usr/local/include/root/TMatrixDUtils.h \
+ /usr/local/include/root/TMatrixDUtilsfwd.h \
+ /usr/local/include/root/TObject.h /usr/local/include/root/TGraphErrors.h \
+ /usr/local/include/root/TGraph.h /usr/local/include/root/TMultiGraph.h \
+ /usr/local/include/root/TF1.h /usr/local/include/root/TLegend.h \
+ /usr/local/include/root/TPave.h /usr/local/include/root/TBox.h
