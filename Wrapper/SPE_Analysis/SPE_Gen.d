3d1f820c52450e4609a19e32ce34f983989f8bed
Watchman-PMT <gary.smith@ed.ac.uk>
2018-11-12 17:41:47 +0000

    Edin Lab progress towards using both digitisers

--- Wrapper/SPE_Analysis/SPE_Gen.d
+++ Wrapper/SPE_Analysis/SPE_Gen.d
@@ -1,7 +1,4 @@
-SPE_Gen.o SPE_Gen.d: SPE_Gen.cpp src/atmap.h src/ns.h src/command_parse.h \
- src/dist.h src/file.h src/stats.h src/utility.h \
- /usr/local/include/root/TFile.h /usr/local/include/root/TDirectoryFile.h \
- /usr/local/include/root/TDirectory.h /usr/local/include/root/TBuffer.h \
+SPE_Gen.o SPE_Gen.d: SPE_Gen.cpp /usr/local/include/root/TApplication.h \
  /usr/local/include/root/TObject.h /usr/local/include/root/RConfigure.h \
  /usr/local/include/root/Rtypes.h /usr/local/include/root/RtypesCore.h \
  /usr/local/include/root/RConfig.h /usr/local/include/root/RVersion.h \
@@ -12,50 +9,41 @@ SPE_Gen.o SPE_Gen.d: SPE_Gen.cpp src/atmap.h src/ns.h src/command_parse.h \
  /usr/local/include/root/TIsAProxy.h \
  /usr/local/include/root/TVirtualIsAProxy.h \
  /usr/local/include/root/TStorage.h \
- /usr/local/include/root/TVersionCheck.h /usr/local/include/root/TNamed.h \
- /usr/local/include/root/TString.h /usr/local/include/root/TMathBase.h \
+ /usr/local/include/root/TVersionCheck.h \
+ /usr/local/include/root/TQObject.h /usr/local/include/root/TError.h \
+ /usr/local/include/root/TList.h /usr/local/include/root/TSeqCollection.h \
+ /usr/local/include/root/TCollection.h \
+ /usr/local/include/root/TIterator.h /usr/local/include/root/TString.h \
+ /usr/local/include/root/TMathBase.h \
  /usr/local/include/root/RStringView.h \
  /usr/local/include/root/RWrap_libcpp_string_view.h \
  /usr/local/include/root/libcpp_string_view.h \
- /usr/local/include/root/TList.h /usr/local/include/root/TSeqCollection.h \
- /usr/local/include/root/TCollection.h \
- /usr/local/include/root/TIterator.h /usr/local/include/root/TDatime.h \
- /usr/local/include/root/TUUID.h /usr/local/include/root/TMap.h \
- /usr/local/include/root/THashTable.h /usr/local/include/root/TUrl.h \
- /usr/local/include/root/ROOT/TRWSpinLock.hxx \
- /usr/local/include/root/ROOT/TSpinMutex.hxx \
- /usr/local/include/root/TH1D.h /usr/local/include/root/TH1.h \
- /usr/local/include/root/TAxis.h /usr/local/include/root/TAttAxis.h \
- /usr/local/include/root/TArrayD.h /usr/local/include/root/TArray.h \
- /usr/local/include/root/TAttLine.h /usr/local/include/root/TAttFill.h \
- /usr/local/include/root/TAttMarker.h /usr/local/include/root/TArrayC.h \
- /usr/local/include/root/TArrayS.h /usr/local/include/root/TArrayI.h \
- /usr/local/include/root/TArrayF.h /usr/local/include/root/Foption.h \
- /usr/local/include/root/TVectorFfwd.h \
- /usr/local/include/root/TVectorDfwd.h \
- /usr/local/include/root/TFitResultPtr.h /usr/local/include/root/TH2D.h \
- /usr/local/include/root/TH2.h /usr/local/include/root/TMatrixFBasefwd.h \
- /usr/local/include/root/TMatrixDBasefwd.h \
- /usr/local/include/root/TGraph.h /usr/local/include/root/TROOT.h \
- src/atmap.h src/command_parse.h src/dist.h src/file.h src/ns.h \
- src/stats.h src/utility.h /usr/local/include/root/TApplication.h \
- /usr/local/include/root/TQObject.h /usr/local/include/root/TError.h \
  /usr/local/include/root/TVirtualQConnection.h \
  /usr/local/include/root/TInterpreter.h \
- /usr/local/include/root/TDictionary.h /usr/local/include/root/ESTLType.h \
+ /usr/local/include/root/TDictionary.h /usr/local/include/root/TNamed.h \
+ /usr/local/include/root/ESTLType.h \
  /usr/local/include/root/TVirtualMutex.h \
  /usr/local/include/root/TApplicationImp.h \
  /usr/local/include/root/TCanvas.h /usr/local/include/root/TPad.h \
  /usr/local/include/root/TVirtualPad.h /usr/local/include/root/TAttPad.h \
- /usr/local/include/root/TVirtualX.h /usr/local/include/root/TAttText.h \
- /usr/local/include/root/GuiTypes.h /usr/local/include/root/Buttons.h \
- /usr/local/include/root/TAttBBox2D.h /usr/local/include/root/TPoint.h \
- /usr/local/include/root/TAttCanvas.h \
+ /usr/local/include/root/TVirtualX.h /usr/local/include/root/TAttLine.h \
+ /usr/local/include/root/TAttFill.h /usr/local/include/root/TAttText.h \
+ /usr/local/include/root/TAttMarker.h /usr/local/include/root/GuiTypes.h \
+ /usr/local/include/root/Buttons.h /usr/local/include/root/TAttBBox2D.h \
+ /usr/local/include/root/TPoint.h /usr/local/include/root/TAttCanvas.h \
  /usr/local/include/root/TCanvasImp.h /usr/local/include/root/TF1.h \
  /usr/local/include/root/TFormula.h /usr/local/include/root/TBits.h \
  /usr/local/include/root/TObjArray.h \
  /usr/local/include/root/TMethodCall.h \
  /usr/local/include/root/Math/ParamFunctor.h \
+ /usr/local/include/root/TH1D.h /usr/local/include/root/TH1.h \
+ /usr/local/include/root/TAxis.h /usr/local/include/root/TAttAxis.h \
+ /usr/local/include/root/TArrayD.h /usr/local/include/root/TArray.h \
+ /usr/local/include/root/TArrayC.h /usr/local/include/root/TArrayS.h \
+ /usr/local/include/root/TArrayI.h /usr/local/include/root/TArrayF.h \
+ /usr/local/include/root/Foption.h /usr/local/include/root/TVectorFfwd.h \
+ /usr/local/include/root/TVectorDfwd.h \
+ /usr/local/include/root/TFitResultPtr.h \
  /usr/local/include/root/TFitResult.h \
  /usr/local/include/root/Fit/FitResult.h \
  /usr/local/include/root/Math/IFunctionfwd.h \
@@ -63,11 +51,21 @@ SPE_Gen.o SPE_Gen.d: SPE_Gen.cpp src/atmap.h src/ns.h src/command_parse.h \
  /usr/local/include/root/TMatrixDSym.h \
  /usr/local/include/root/TMatrixTSym.h \
  /usr/local/include/root/TMatrixTBase.h \
+ /usr/local/include/root/TMatrixFBasefwd.h \
+ /usr/local/include/root/TMatrixDBasefwd.h \
  /usr/local/include/root/TMatrixTUtils.h \
  /usr/local/include/root/TMatrixDSymfwd.h \
  /usr/local/include/root/TMatrixDUtils.h \
  /usr/local/include/root/TMatrixDUtilsfwd.h \
- /usr/local/include/root/TObject.h /usr/local/include/root/TGraphErrors.h \
- /usr/local/include/root/TGraph.h /usr/local/include/root/TMultiGraph.h \
- /usr/local/include/root/TF1.h /usr/local/include/root/TLegend.h \
- /usr/local/include/root/TPave.h /usr/local/include/root/TBox.h
+ /usr/local/include/root/TObject.h /usr/local/include/root/TFile.h \
+ /usr/local/include/root/TDirectoryFile.h \
+ /usr/local/include/root/TDirectory.h /usr/local/include/root/TBuffer.h \
+ /usr/local/include/root/TDatime.h /usr/local/include/root/TUUID.h \
+ /usr/local/include/root/TMap.h /usr/local/include/root/THashTable.h \
+ /usr/local/include/root/TUrl.h \
+ /usr/local/include/root/ROOT/TRWSpinLock.hxx \
+ /usr/local/include/root/ROOT/TSpinMutex.hxx \
+ /usr/local/include/root/TGraphErrors.h /usr/local/include/root/TGraph.h \
+ /usr/local/include/root/TMultiGraph.h /usr/local/include/root/TF1.h \
+ /usr/local/include/root/TLegend.h /usr/local/include/root/TPave.h \
+ /usr/local/include/root/TBox.h /usr/local/include/root/TGraph.h
