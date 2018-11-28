d60b031bdaa5aa96ed27db0dbd5f199054e60da2
gsmith23 <gsmith23@ph.ed.ac.uk>
2018-11-28 20:55:37 +0000

    Tidying

---
+++ Wrapper/DataSize.C
@@ -0,0 +1,33 @@
+double BytesPerSecond(char   test = 'A',
+		      double acquisitionTime = 15.
+		      ){
+  
+  // Trigger Rate
+  double Tr = 680.;
+  // Record Length
+  double Rl = 5100;
+  //  Bytes per sample
+  double Bs = 2.;
+  // Number of channels
+  double Nc = 1.;
+  // Bytes per header
+  double Bh = 16.;
+
+  if( test!='A' ){
+    Tr = 10000.;
+    Rl = 102.;
+  }
+  
+  return Tr*(Rl*Bs*Nc+Bh);
+  
+}
+
+double DataSize(char   test,
+		double acquisitionTime){
+  
+  // convert to seconds
+  acquisitionTime *= 60.;
+  
+  return acquisitionTime*BytesPerSecond(test);
+  
+}
