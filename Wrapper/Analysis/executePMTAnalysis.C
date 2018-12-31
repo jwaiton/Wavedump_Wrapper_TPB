558fddbb98452986d97411cbf3381e3c10d67b0d
gsmith23 <gsmith23@ph.ed.ac.uk>
2018-12-31 20:52:27 +0000

    Main function now accepts file path/s as argument

--- Wrapper/Analysis/executePMTAnalysis.C
+++ Wrapper/Analysis/executePMTAnalysis.C
@@ -1,29 +1,52 @@
+#include<iostream>
+
+#include <string>
 #include "TFile.h"
 #include "TTree.h"
+#include "TString.h"
 #include "PMTAnalyser.h"
+#include "fnParser.h"
+
+using namespace std;
+
 
 int main(Int_t argc, Char_t *argv[]){
   
-  TString testName = "Run_11_PMT_90_Loc_0_Test_D";
-  
-  TString fileName = "../../RawRootData/" + testName;
-  fileName += ".root";
-  
-  TString treeName = "Events_" + testName;
+  //string filePath = "../../RawRootData/Run_11_PMT_90_Loc_0_Test_D.root";
 
-  TFile *file = new TFile(fileName,"READ");
+  TFile *file = nullptr;
+  TTree * tree = nullptr;
   
-  if (!file || !file->IsOpen()) {
-    file = new TFile();
-    return -1;
+  PMTAnalyser * analysePMT = nullptr;
+  
+  for( int iFile = 1 ; iFile < argc ; iFile++){
+     
+     file = new TFile(argv[iFile],"READ");
+     
+     if (!file || !file->IsOpen()) {
+       file = new TFile();
+   
+       cerr << " File Error " << endl; 
+       return -1;
+     }
+
+     //-------
+     cout << endl;
+     cout << " Run      " << run(argv[iFile])   << endl;
+     cout << " PMT      " << pmtID(argv[iFile]) << endl;
+     cout << " Location " << location(argv[iFile]) << endl;
+
+//      cout << GetFileName(argv[iFile]) << endl;
+//      cout << GetTreeName(argv[iFile]) << endl;
+     
+     file->GetObject((TString)GetTreeName(argv[iFile]),tree); 
+     
+     analysePMT = new PMTAnalyser(tree);
+     
+     analysePMT->Loop();
+  
   }
-  
-  TTree * tree;
-  file->GetObject(treeName,tree); 
-    
-  PMTAnalyser * analysePMT = new PMTAnalyser(tree);
-  
-  analysePMT->Loop();
 
   
+  return 1;
 }
