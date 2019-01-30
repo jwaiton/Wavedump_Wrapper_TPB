b41a422b375619ec056bfe55a7891f351ad00ed6
gsmith23 <gsmith23@ph.ed.ac.uk>
2019-01-30 21:04:49 +0000

    input compatible with BinToRoot output

--- Wrapper/Analysis/Fitting/runGetPeakToValley.sh
+++ Wrapper/Analysis/Fitting/runGetPeakToValley.sh
@@ -1,2 +1,7 @@
-( sleep 10 ; echo .q  ) | root -l 'GetPeakToValley.C++("./outputFile.root",1)';
-open ./Plots/*.pdf;
+( sleep 10 ; echo .q  ) | root -l 'GetPeakToValley.C++("Run_200_PMT_15_Loc_0_Test_S")';
+
+# mac command to open plot 
+# open ./Plots/*.pdf;
+
+# linux try:
+# okular ./Plots/*.pdf
