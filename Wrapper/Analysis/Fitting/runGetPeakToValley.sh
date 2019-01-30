3809613351f7280595d80e568d2587f23a1efad7
gsmith23 <gsmith23@ph.ed.ac.uk>
2019-01-30 21:01:54 +0000

    Modified for use with BinToRoot output

--- Wrapper/Analysis/Fitting/runGetPeakToValley.sh
+++ Wrapper/Analysis/Fitting/runGetPeakToValley.sh
@@ -1,2 +1,2 @@
-( sleep 10 ; echo .q  ) | root -l 'GetPeakToValley.C++("./outputFile.root",1)';
+( sleep 10 ; echo .q  ) | root -l 'GetPeakToValley.C++("Run_200_PMT_15_Loc_0_Test_S")';
 open ./Plots/*.pdf;
