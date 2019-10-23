0544890171f04bff326b0c1230b5aa208eea00ab
Watchman-PMT <gary.smith@ed.ac.uk>
2019-10-23 23:36:21 +0100

    fixed bug

--- running.sh
+++ running.sh
@@ -16,6 +16,12 @@ cd $WM_COOK
 ./make_plot_directories.sh
 cook_raw ${FILE_PATH}wave_0.dat.root
 
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+evince ${WM_COOK}Plots/DAQ/hEventRate.pdf &
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+open ${WM_COOK}Plots/DAQ/hEventRate.pdf &
+fi
+
 echo " ------------------------------"
 date 
 echo " ------------------------------"
@@ -31,9 +37,10 @@ echo " ------------------------------"
 cd $WM_CODE
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-evince ${WM_COOK}Plots/DAQ/hEventRate.pdf &
-evince ${WM_ANALYSE_COOKED}Plots/Noise/hPeak_Cooked.pdf 
+evince ${WM_ANALYSE_COOKED}Plots/Noise/hPeak_Cooked.pdf &
+evince ${WM_ANALYSE_COOKED}Plots/Waveforms/hWaveFFT.pdf &
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-open ${WM_COOK}Plots/DAQ/hEventRate.pdf &
-open ${WM_ANALYSE_COOKED}Plots/Timing/hPeak_Cooked.pdf
+open ${WM_ANALYSE_COOKED}Plots/Noise/hPeak_Cooked.pdf &
+open ${WM_ANALYSE_COOKED}Plots/Waveforms/hWaveFFT.pdf & 
 fi
+
