b9eb92461ea65a27c44be9a32e133872e2a8112d
gsmith23 <gsmith23@ph.ed.ac.uk>
2020-03-04 18:20:22 +0000

    shell script can run from anywhere, use in folder local to data

---
+++ process_analyse_locally_auto.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo " ------------------------------"
+date  
+echo " ------------------------------"
+
+echo " Enter full path to binary file, e.g.: " 
+echo "/home/user1/Watchman/Wavedump_Wrapper/Data_Storage/Binary_Data/Setup/RUN000002/PMT0015/Nominal/" 
+
+DIR_PATH=${PWD}/
+
+FILE_NAME=$(find . -maxdepth 1 -name "wave_*.dat" )
+
+FILE_PATH=${DIR_PATH}${FILE_NAME}
+
+dat_to_root ${FILE_PATH}
+
+echo " ------------------------------"
+date 
+echo " ------------------------------"
+
+mkdir -p ./Plots/DAQ/
+
+cook_raw ${FILE_PATH}.root
+
+echo " ------------------------------"
+date 
+echo " ------------------------------"
+
+mkdir -p ./Plots/Noise/
+mkdir -p ./Plots/Timing/
+mkdir -p ./Plots/Dark/
+
+analyse_cooked  ${DIR_PATH}/Run*
+
+echo " ------------------------------"
+date 
+echo " ------------------------------"
+
+mkdir -p ./Plots/Waveforms/
