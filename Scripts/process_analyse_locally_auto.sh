2eb343aca52c0a7bf4f794ac90ab6dfb1675e705
Watchman-PMT <gary.smith@ed.ac.uk>
2022-03-15 13:12:58 +0000

    new style by Tom and Matt, similar method

--- Scripts/process_analyse_locally_auto.sh
+++ Scripts/process_analyse_locally_auto.sh
@@ -8,26 +8,33 @@ echo " -------------------------------"
 
 DIR_PATH=${PWD}/
 
-FILE_NAME=$(find . -maxdepth 1 -name "wave_*.dat" )
+FILE_NAME=$(find . -maxdepth 3 -name "wave_*.dat" )
 
-FILE_PATH=${DIR_PATH}${FILE_NAME}
+for f in $FILE_NAME
+do
+     FILE_PATH=${DIR_PATH}${FILE_NAME}
+     echo "**************************"
+     echo $FILE_PATH
+     echo $FILE_NAME
+     echo "*************************"
 
-dat_to_root ${FILE_PATH}
+     dat_to_root ${FILE_PATH}
 
-echo " ------------------------------"
-date 
-echo " ------------------------------"
+     echo " ------------------------------"
+     date 
+     echo " ------------------------------"
 
-mkdir -p ./Plots/DAQ/
+     mkdir -p ./Plots/DAQ/
 
-cook_raw ${FILE_PATH}.root
+     cook_raw ${FILE_PATH}.root
 
-echo " ------------------------------"
-date 
-echo " ------------------------------"
+     echo " ------------------------------"
+     date 
+     echo " ------------------------------"
 
-analyse_cooked  ${DIR_PATH}/Run*
+     analyse_cooked  ${DIR_PATH}/Run*
 
-echo " ------------------------------"
-date 
-echo " ------------------------------"
+     echo " ------------------------------"
+     date 
+     echo " ------------------------------"
+done
