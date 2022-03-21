5fefe326b4f5ca4fbd78586ca1ef610ff785f5b7
Watchman-PMT <gary.smith@ed.ac.uk>
2022-03-21 14:00:46 +0000

    Not ready for testing ye

---
+++ Acquisition/Noise/Dark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Shell script for acquiring data for
+#   Noise test
+
+# wavedump settings (including e.g. no. samples per waveform)   
+export config_file="${WM_DAQ}Config/Conf_Dark.txt"
+
+#export test="test"
+export test="dark"
+
+# run common acquisition program which
+# will utilise the settings above
+source $WM_DAQ/Acquire.sh
