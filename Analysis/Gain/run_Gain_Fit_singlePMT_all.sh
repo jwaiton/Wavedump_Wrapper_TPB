ff5638f58600ef6c7098b130453e77b2a0b61230
Watchman-PMT <gary.smith@ed.ac.uk>
2022-03-21 17:57:55 +0000

    four commands in one

---
+++ Analysis/Gain/run_Gain_Fit_singlePMT_all.sh
@@ -0,0 +1,7 @@
+
+HISTOPATH="/home/user1/Watchman/Wavedump_Wrapper/Storage/17mar22/Histograms/"
+
+(echo '171' && echo '4' && echo '3') | ./Gain_Fit_singlePMT $HISTOPATH
+(echo '131' && echo '5' && echo '3') | ./Gain_Fit_singlePMT $HISTOPATH 
+(echo '107' && echo '6' && echo '3') | ./Gain_Fit_singlePMT $HISTOPATH 
+(echo '166' && echo '7' && echo '3') | ./Gain_Fit_singlePMT $HISTOPATH 
