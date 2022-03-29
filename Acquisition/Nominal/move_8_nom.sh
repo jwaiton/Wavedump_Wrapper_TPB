e063af28a3c4f0f9af862cf12c6b05bae40d01d9
Watchman-PMT <gary.smith@ed.ac.uk>
2022-03-29 11:13:40 +0100

    more tools to support data acquisition and processing

--- Acquisition/Nominal/move_8_nom.sh
+++ Acquisition/Nominal/move_8_nom.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-PARENT=/home/user1/Watchman/Wavedump_Wrapper/Storage/21mar22/
-
 # move trigger data (as PMT0000) 
+
+
+
+export FOLDER_NAME=""
+source ../read_folder_name.sh
+
+echo 'FOLDER_NAME = ' $FOLDER_NAME
 
 echo 'Which RUN ? ( e.g. 000001 )'
 read RUN
 
-PARENT=${PARENT}RUN${RUN}/
+PARENT=${FOLDER_NAME}RUN${RUN}/
 
 echo $PARENT
 
@@ -19,7 +24,5 @@ cd $FOLDER
 
 ## process pulser data (no analysis)
 # process_locally_auto.sh > output.txt
-
-
-    
+  
 
