ec11c37d3df2063dcbb0ab057cc01449a7c0bca6
WATCHMAM-PMT <gary.smith@ed.ac.uk>
2022-03-30 16:10:25 +0100

    incremental progress

--- Analysis/Results/CSVToRoot.C
+++ Analysis/Results/CSVToRoot.C
@@ -5,37 +5,46 @@
 #include "TTree.h"
 #include "TCanvas.h"
 
+#include "wmStyle.C"
+#include "ShippingData.h"
+#include "dark_rate_at_temp.h"
+
 void CSVToRoot(){
   
   TFile *outFile = new TFile("Results.root","RECREATE");
+
+  TStyle *wmStyle = GetwmStyle();
+  wmStyle->SetOptFit(1);
+  gROOT->SetStyle("wmStyle");
+  gROOT->ForceStyle();
   
-  TTree *T = new TTree("Results","Results from CSV file");
+  TTree *Results = new TTree("Results","Results from CSV file");
   Long64_t nlines;
 
   //PMT Number, Rig Location, Nominal Voltage [V],Temperature, Dark Rate [Hz] Short Run, Scaled HPK Dark Rate [Hz],
   //Mu,	P:V, HPK P:V, Gain [x10^7], Operating Voltage [V], ∆V, Dark Rate [Hz]  Long Run
   TString lineFormat = "PMT/I:Loc/I:HV_H/F:Temp/F:Dark_S/F:Dark_H/F:Mu/F:PTV/F:PTV_H/F:Gain/F:HV/F:dV/F:Dark/F";
 
-  nlines = T->ReadFile("Results.csv",lineFormat,',');
+  nlines = Results->ReadFile("Results.csv",lineFormat,',');
 
   int   PMT, Loc;
   float HV_H, Temp, Dark_S, Dark_H, Mu, PTV, PTV_H, Gain, HV, dV, Dark;
-  T->SetBranchAddress("PMT",&PMT);
-  T->SetBranchAddress("Loc",&Loc);
+  Results->SetBranchAddress("PMT",&PMT);
+  Results->SetBranchAddress("Loc",&Loc);
 
-  T->SetBranchAddress("HV_H",&HV_H);
-  T->SetBranchAddress("Temp",&Temp);
-  T->SetBranchAddress("Dark_S",&Dark_S);
-  T->SetBranchAddress("Dark_H",&Dark_H);
+  Results->SetBranchAddress("HV_H",&HV_H);
+  Results->SetBranchAddress("Temp",&Temp);
+  Results->SetBranchAddress("Dark_S",&Dark_S);
+  Results->SetBranchAddress("Dark_H",&Dark_H);
 
-  T->SetBranchAddress("Mu",&Mu);
-  T->SetBranchAddress("PTV",&PTV);
-  T->SetBranchAddress("PTV_H",&PTV_H);
-  T->SetBranchAddress("Gain",&Gain);
+  Results->SetBranchAddress("Mu",&Mu);
+  Results->SetBranchAddress("PTV",&PTV);
+  Results->SetBranchAddress("PTV_H",&PTV_H);
+  Results->SetBranchAddress("Gain",&Gain);
 
-  T->SetBranchAddress("HV",&HV);
-  T->SetBranchAddress("dV",&dV);
-  T->SetBranchAddress("Dark",&Dark);
+  Results->SetBranchAddress("HV",&HV);
+  Results->SetBranchAddress("dV",&dV);
+  Results->SetBranchAddress("Dark",&Dark);
   
   printf(" Results for %lld PMTs \n",nlines);
 
@@ -45,40 +54,31 @@ void CSVToRoot(){
   c1->Divide(5,2);
 
   c1->cd(1);
-  T->Draw("Temp");
+  Results->Draw("Temp");
     
   c1->cd(2);
-  T->Draw("Dark_S");
+  Results->Draw("Dark_S");
 
   c1->cd(3);
-  T->Draw("Dark");
+  Results->Draw("Dark");
 
   c1->cd(4);
-  T->Draw("Dark_H:Dark","","colz");
+  Results->Draw("Dark_H:Dark","","colz");
 
   c1->cd(5);
-  T->Draw("PTV");
+  Results->Draw("PTV");
 
   c1->cd(6);
-  T->Draw("Gain");
+  Results->Draw("Gain");
  
-
   TH1F   *h1 = new TH1F("h1","x distribution;X LABEL; Counts",100,0,5000);
 
   double Dark_arr[30];
   double Dark_H_arr[30];
+
+
+  //Results->cd();
   
-  for (int i = 0 ; i < T->GetEntries() ; i++){
-    T->GetEntry(i);
-
-    Dark_arr[i] = Dark;
-    Dark_H_arr[i] = Dark_H;
-    
-    cout << " Dark = " << Dark << endl;
-    
-    h1->Fill(Dark);
-  }
-
   c1->cd(7);
   h1->Draw();
   h1->Write();
@@ -93,15 +93,13 @@ void CSVToRoot(){
   g1->SetMarkerStyle(20);
   g1->Draw("AP");
 
-  T->Write();
-
   TH1F *W1 = new TH1F("TW1","PTV comparison;PTV;PTV_H",30,1,4);
 
   double PTV_arr[30];
   double PTV_H_arr[30];
 
-  for (int i = 0 ; i < T->GetEntries() ; i++){
-    T->GetEntry(i);
+  for (int i = 0 ; i < Results->GetEntries() ; i++){
+    Results->GetEntry(i);
 
     PTV_arr[i] = PTV;
     PTV_H_arr[i] = PTV_H;
@@ -125,7 +123,27 @@ void CSVToRoot(){
   g2->SetMarkerStyle(20);
   g2->Draw("AP");
 
-  T->Write();
+  Results->Write();
 
+    ShippingData *ship_data = new ShippingData(0);
+  
+  for (int i = 0 ; i < Results->GetEntries() ; i++){
+    Results->GetEntry(i);
+    
+    Dark_arr[i] = Dark;
+    Dark_H_arr[i] = Dark_H;
 
+    ship_data = new ShippingData(PMT,0);
+    
+    if( (int)Dark_H != (int)ship_data->GetDR(Temp) ){
+	cout << " HPK Dark scaled ( function ) = "
+	     << ship_data->GetDR(Temp) << endl;
+	cout << " HPK Dark scaled ( CSV file  ) = "
+	     << Dark_H  << endl;
+      }
+      
+    h1->Fill(Dark);
+    delete ship_data;
+    
+  }
 }
