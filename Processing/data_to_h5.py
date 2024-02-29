04763358a05626ddd34bb7da4c51936b7f2822c7
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2024-02-29 16:22:57 +0000

    altering data_to_h5.py to work with arguments at the command line (WIP! Currently broken!)

--- Processing/data_to_h5.py
+++ Processing/data_to_h5.py
@@ -12,7 +12,7 @@ import numpy as np
 import h5py
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import argparse
 
 import sys,os,os.path
 
@@ -23,12 +23,35 @@ if module_path not in sys.path:
 
 from Analyse.core import processing as proc
 
-def main(file_path):
-    proc.read_raw_h5(file_path, verbose = False, save_h5=True, print_mod = 10000)
+
+
+'''
+argument list:
+1 - file path
+2 - save_h5
+3 - cook flag
+4 - verbose flag
+5 - printing modifier
+'''
+parser = argparse.ArgumentParser(description='Process wavedump .dat data.')
+
+parser.add_argument('filepath')
+parser.add_argument('-h5', action='store_true')
+parser.add_argument('-c', '--cook', action='store_true')
+parser.add_argument('-v', '--verbose', action='store_true')
+parser.add_argument('-p', '--print', choices=range(1,1000000))
+
+args = parser.parse_args()
+    
+
+def main(arguments):
+
+    print(args.filepath, args.h5, args.c, args.v, args.p)
+    proc.read_raw_h5(args.filepath, args.h5, args.c, args.v, args.p)
 
 
 if __name__ == "__main__":
     # this is it taking the file directory from the first argument,
     # which is generally passed through in process_analyse_locally_auto.sh
     print("Processing file at:\n{}".format(sys.argv[1]))
-    main(sys.argv[1])
+    main(sys.argv)
